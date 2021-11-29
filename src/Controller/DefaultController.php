9510570655b30057024356ab90f6a943bb9d8592
clr-89 <clr.minier@gmail.com>
2021-11-29 21:25:03 +0100

    webpack

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+
+    public function index() : Response
+    {
+        return $this->render('program/index.html.twig');
+    }
+}
