511ba77a201e70ab158e6f0ffb3e5899cedea127
clr-89 <clr.minier@gmail.com>
2021-12-01 22:00:00 +0100

    symfony 8

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,15 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-
+/**
+ * @Route("/", name="app_")
+ */
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("", name="index")
      */
 
     public function index() : Response
     {
-        return $this->render('program/index.html.twig');
+        return $this->render('home/index.html.twig');
     }
 }
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,20 +15,37 @@ class ProgramController extends \Symfony\Bundle\FrameworkBundle\Controller\Abstr
 {
     /**
      * @Route("/", name="index")
+     * @return Response A response instance
      */
+
     public function index(): Response
     {
-        return $this->render('program/index.html.twig', [
-            'website' => 'Wild Séries',
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+        return $this->render(
+            'program/index.html.twig', [
+                'programs' => $programs,
         ]);
     }
-    /**
-    * @Route("/{id}", methods={"GET"}, requirements={"id"="\d+"}, name="id")
+
+     /**
+     * @Route("/show/{id<^[0-9]+$>}", name="show")
+     * @return Response
      */
     public function show(int $id): Response
     {
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
         return $this->render('program/show.html.twig', [
-            'id'  => $id,
+            'program' => $program,
         ]);
     }
 }
